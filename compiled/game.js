d37826c8daffb2f0d62fef1d48027703bf6fa57f
Kyle Shipley <shipstar@gmail.com>
2011-09-11 16:41:44 -0400

    Make ship actually move. Create a basic game loop.

--- compiled/game.js
+++ compiled/game.js
@@ -1,21 +1,40 @@
 (function() {
   $(function() {
-    var canvas, spaceship;
-    canvas = $('#canvas');
-    spaceship = {
-      width: 20,
-      height: 20,
-      x: canvas.width() / 2,
-      y: 550
+    var canvas, clearCanvas, context, drawShip, gameLoop, init, ship, updateShip;
+    canvas = null;
+    context = null;
+    ship = null;
+    clearCanvas = function() {
+      return context.clearRect(0, 0, canvas.width(), canvas.height());
     };
-    canvas.get(0).getContext("2d").fillRect(spaceship.x, spaceship.y, spaceship.width, spaceship.height);
-    canvas.get(0).getContext("2d").fillRect(spaceship.x + spaceship.width / 2 - 1, spaceship.y - 4, 2, 4);
-    return $(document).keydown(function(event) {
+    drawShip = function(ship) {
+      canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height);
+      return canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4);
+    };
+    init = function() {
+      canvas = $("#canvas");
+      context = canvas.get(0).getContext("2d");
+      ship = {
+        width: 20,
+        height: 20,
+        x: canvas.width() / 2,
+        y: 550,
+        movementInterval: 10
+      };
+      return setInterval(gameLoop, 17);
+    };
+    gameLoop = function() {
+      clearCanvas();
+      return drawShip(ship);
+    };
+    updateShip = function(event) {
       if (event.which === $.ui.keyCode.LEFT) {
-        return console.log('pressed left!');
+        return ship.x -= ship.movementInterval;
       } else if (event.which === $.ui.keyCode.RIGHT) {
-        return console.log('pressed right.');
+        return ship.x += ship.movementInterval;
       }
-    });
+    };
+    $(document).keydown(updateShip);
+    return init();
   });
 }).call(this);
--- src/game.coffee
+++ src/game.coffee
@@ -1,15 +1,34 @@
 $ ->
-  canvas = $('#canvas')
-  spaceship = { width: 20, height: 20, x: canvas.width() / 2, y: 550 }
-  canvas.get(0).getContext("2d").fillRect(spaceship.x, spaceship.y, spaceship.width, spaceship.height)
-  canvas.get(0).getContext("2d").fillRect(spaceship.x + spaceship.width / 2 - 1, spaceship.y - 4, 2, 4)
-  $(document).keydown (event) ->
+  canvas = null
+  context = null
+  ship = null
+
+  clearCanvas = ->
+    context.clearRect(0, 0, canvas.width(), canvas.height())
+
+  drawShip = (ship) ->
+    canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height)
+    canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4) 
+
+  init = ->
+    canvas = $("#canvas")
+    context = canvas.get(0).getContext("2d")
+    ship = { width: 20, height: 20, x: canvas.width() / 2, y: 550, movementInterval: 10 }
+    setInterval(gameLoop, 17)
+  
+  gameLoop = ->
+    clearCanvas()
+    drawShip(ship)
+
+  updateShip = (event) ->
     if event.which == $.ui.keyCode.LEFT
-      console.log('pressed left!')
-      #spaceship.x -= 1
+      ship.x -= ship.movementInterval
     else if event.which == $.ui.keyCode.RIGHT
-      #spaceship.x += 1
-      console.log('pressed right.')
+      ship.x += ship.movementInterval
 
+  $(document).keydown(updateShip)
 
-    
+  init()
+
+  
+  
