9d597d40ffbd9862cae3260ae7b206d7e7e60076
Kyle Shipley <shipstar@gmail.com>
2011-09-11 17:04:40 -0400

    Shoot bullets

--- compiled/game.js
+++ compiled/game.js
@@ -1,15 +1,55 @@
 (function() {
+  Array.prototype.remove = function(value) {
+    var i;
+    i = 0;
+    while (i < this.length) {
+      if (this[i] === value) {
+        this.splice(i, 1);
+      } else {
+        ++i;
+      }
+    }
+    return this;
+  };
   $(function() {
-    var canvas, clearCanvas, context, drawShip, gameLoop, init, ship, updateShip;
+    var bullets, canvas, clearCanvas, context, drawBullets, drawShip, gameLoop, init, ship, updateBullets, updateShip;
     canvas = null;
     context = null;
     ship = null;
+    bullets = [];
     clearCanvas = function() {
       return context.clearRect(0, 0, canvas.width(), canvas.height());
     };
     drawShip = function(ship) {
       canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height);
       return canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4);
+    };
+    updateBullets = function(bullets) {
+      var bullet, _i, _len;
+      for (_i = 0, _len = bullets.length; _i < _len; _i++) {
+        bullet = bullets[_i];
+        bullet.y += bullet.velocity;
+      }
+      return bullets = (function() {
+        var _j, _len2, _results;
+        _results = [];
+        for (_j = 0, _len2 = bullets.length; _j < _len2; _j++) {
+          bullet = bullets[_j];
+          if (bullet.y > 0) {
+            _results.push(bullet);
+          }
+        }
+        return _results;
+      })();
+    };
+    drawBullets = function(bullets) {
+      var bullet, _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = bullets.length; _i < _len; _i++) {
+        bullet = bullets[_i];
+        _results.push(canvas.get(0).getContext("2d").fillRect(bullet.x, bullet.y, bullet.width, bullet.height));
+      }
+      return _results;
     };
     init = function() {
       canvas = $("#canvas");
@@ -24,14 +64,24 @@
       return setInterval(gameLoop, 17);
     };
     gameLoop = function() {
+      updateBullets(bullets);
       clearCanvas();
-      return drawShip(ship);
+      drawShip(ship);
+      return drawBullets(bullets);
     };
     updateShip = function(event) {
       if (event.which === $.ui.keyCode.LEFT) {
         return ship.x -= ship.movementInterval;
       } else if (event.which === $.ui.keyCode.RIGHT) {
         return ship.x += ship.movementInterval;
+      } else if (event.which === $.ui.keyCode.SPACE) {
+        return bullets.push({
+          width: 2,
+          height: 2,
+          x: ship.x + ship.width / 2,
+          y: ship.y - 1,
+          velocity: -15
+        });
       }
     };
     $(document).keydown(updateShip);
--- src/game.coffee
+++ src/game.coffee
@@ -1,7 +1,17 @@
+Array::remove = (value) ->
+    i = 0
+    while i < @length
+        if @[i] == value
+            @splice i, 1
+        else
+            ++i
+    return @
+
 $ ->
   canvas = null
   context = null
   ship = null
+  bullets = []
 
   clearCanvas = ->
     context.clearRect(0, 0, canvas.width(), canvas.height())
@@ -9,6 +19,16 @@ $ ->
   drawShip = (ship) ->
     canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height)
     canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4) 
+  
+  updateBullets = (bullets) ->
+    for bullet in bullets
+      bullet.y += bullet.velocity
+    
+    bullets = (bullet for bullet in bullets when bullet.y > 0)
+
+  drawBullets = (bullets) ->
+    for bullet in bullets
+      canvas.get(0).getContext("2d").fillRect(bullet.x, bullet.y, bullet.width, bullet.height)
 
   init = ->
     canvas = $("#canvas")
@@ -17,18 +37,19 @@ $ ->
     setInterval(gameLoop, 17)
   
   gameLoop = ->
+    updateBullets(bullets)
     clearCanvas()
     drawShip(ship)
+    drawBullets(bullets)
 
   updateShip = (event) ->
     if event.which == $.ui.keyCode.LEFT
       ship.x -= ship.movementInterval
     else if event.which == $.ui.keyCode.RIGHT
       ship.x += ship.movementInterval
+    else if event.which == $.ui.keyCode.SPACE
+      bullets.push { width: 2, height: 2, x: ship.x + ship.width / 2, y: ship.y - 1, velocity: -15 }
 
   $(document).keydown(updateShip)
 
   init()
-
-  
-  
