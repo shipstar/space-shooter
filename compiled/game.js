812fb7923965ddda062dfb58740e5549d8294d36
Anthony Panozzo <panozzaj@gmail.com>
2011-09-11 16:22:19 -0400

    Commit some key events.

--- compiled/game.js
+++ compiled/game.js
@@ -1,7 +1,21 @@
 (function() {
   $(function() {
-    return $('#stage').click(function() {
-      return this.getContext("2d").fillRect(50, 25, 150, 100);
+    var canvas, spaceship;
+    canvas = $('#canvas');
+    spaceship = {
+      width: 20,
+      height: 20,
+      x: canvas.width() / 2,
+      y: 550
+    };
+    canvas.get(0).getContext("2d").fillRect(spaceship.x, spaceship.y, spaceship.width, spaceship.height);
+    canvas.get(0).getContext("2d").fillRect(spaceship.x + spaceship.width / 2 - 1, spaceship.y - 4, 2, 4);
+    return $(document).keydown(function(event) {
+      if (event.which === $.ui.keyCode.LEFT) {
+        return console.log('pressed left!');
+      } else if (event.which === $.ui.keyCode.RIGHT) {
+        return console.log('pressed right.');
+      }
     });
   });
 }).call(this);
--- src/game.coffee
+++ src/game.coffee
@@ -1,3 +1,15 @@
 $ ->
-  $('#stage').click ->
-    this.getContext("2d").fillRect(50, 25, 150, 100)
+  canvas = $('#canvas')
+  spaceship = { width: 20, height: 20, x: canvas.width() / 2, y: 550 }
+  canvas.get(0).getContext("2d").fillRect(spaceship.x, spaceship.y, spaceship.width, spaceship.height)
+  canvas.get(0).getContext("2d").fillRect(spaceship.x + spaceship.width / 2 - 1, spaceship.y - 4, 2, 4)
+  $(document).keydown (event) ->
+    if event.which == $.ui.keyCode.LEFT
+      console.log('pressed left!')
+      #spaceship.x -= 1
+    else if event.which == $.ui.keyCode.RIGHT
+      #spaceship.x += 1
+      console.log('pressed right.')
+
+
+    
