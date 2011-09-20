29deeed9413fc470030849b84b682f7bc6088524
Anthony Panozzo <panozzaj@gmail.com>
2011-09-19 20:51:30 -0400

    Add basic target addition.

--- compiled/game.js
+++ compiled/game.js
@@ -12,17 +12,35 @@
     return this;
   };
   $(function() {
-    var bullets, canvas, clearCanvas, context, drawBullets, drawShip, gameLoop, handleKeys, init, ship, updateBullets, updateShip;
+    var bullets, canvas, clearCanvas, context, drawBullets, drawShip, drawTargets, gameLoop, generateTarget, handleKeys, init, ship, targets, updateBullets, updateShip;
     canvas = null;
     context = null;
     ship = null;
     bullets = [];
-    clearCanvas = function() {
-      return context.clearRect(0, 0, canvas.width(), canvas.height());
+    targets = [];
+    init = function() {
+      canvas = $("#canvas");
+      context = canvas.get(0).getContext("2d");
+      ship = {
+        width: 20,
+        height: 20,
+        x: canvas.width() / 2,
+        y: canvas.height() - 50,
+        movementInterval: 10,
+        firing: false,
+        movingLeft: false,
+        movingRight: false
+      };
+      return setInterval(gameLoop, 17);
     };
-    drawShip = function(ship) {
-      canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height);
-      return canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4);
+    gameLoop = function() {
+      updateBullets();
+      updateShip(ship);
+      generateTarget();
+      clearCanvas();
+      drawShip(ship);
+      drawBullets(bullets);
+      return drawTargets(targets);
     };
     updateShip = function(ship) {
       if (ship.movingLeft) {
@@ -65,6 +83,25 @@
         return _results;
       })();
     };
+    generateTarget = function() {
+      var targetWidth;
+      if (Math.random() < 0.01) {
+        targetWidth = 30;
+        return targets.push({
+          width: targetWidth,
+          height: 30,
+          x: Math.random() * canvas.width() - targetWidth,
+          y: 30
+        });
+      }
+    };
+    clearCanvas = function() {
+      return context.clearRect(0, 0, canvas.width(), canvas.height());
+    };
+    drawShip = function(ship) {
+      canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height);
+      return canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4);
+    };
     drawBullets = function(bullets) {
       var bullet, _i, _len, _results;
       _results = [];
@@ -74,27 +111,14 @@
       }
       return _results;
     };
-    init = function() {
-      canvas = $("#canvas");
-      context = canvas.get(0).getContext("2d");
-      ship = {
-        width: 20,
-        height: 20,
-        x: canvas.width() / 2,
-        y: canvas.height() - 50,
-        movementInterval: 10,
-        firing: false,
-        movingLeft: false,
-        movingRight: false
-      };
-      return setInterval(gameLoop, 17);
-    };
-    gameLoop = function() {
-      updateBullets();
-      updateShip(ship);
-      clearCanvas();
-      drawShip(ship);
-      return drawBullets(bullets);
+    drawTargets = function(targets) {
+      var target, _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = targets.length; _i < _len; _i++) {
+        target = targets[_i];
+        _results.push(canvas.get(0).getContext("2d").fillRect(target.x, target.y, target.width, target.height));
+      }
+      return _results;
     };
     handleKeys = function(options) {
       return function() {
--- src/game.coffee
+++ src/game.coffee
@@ -12,30 +12,7 @@ $ ->
   context = null
   ship = null
   bullets = []
-
-  clearCanvas = ->
-    context.clearRect(0, 0, canvas.width(), canvas.height())
-
-  drawShip = (ship) ->
-    canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height)
-    canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4)
-
-  updateShip = (ship) ->
-    if ship.movingLeft
-      ship.x -= ship.movementInterval if ship.x > 0
-    if ship.movingRight
-      ship.x += ship.movementInterval if (ship.x + ship.width) < canvas.width()
-    if ship.firing
-      bullets.push { width: 2, height: 2, x: ship.x + ship.width / 2, y: ship.y - 1, velocity: -15 } unless bullets.length > 4
-
-  updateBullets = ->
-    for bullet in bullets
-      bullet.y += bullet.velocity
-    bullets = (bullet for bullet in bullets when bullet.y > 0)
-
-  drawBullets = (bullets) ->
-    for bullet in bullets
-      canvas.get(0).getContext("2d").fillRect(bullet.x, bullet.y, bullet.width, bullet.height)
+  targets = []
 
   init = ->
     canvas = $("#canvas")
@@ -53,11 +30,51 @@ $ ->
     setInterval(gameLoop, 17)
 
   gameLoop = ->
+    # game logic
     updateBullets()
     updateShip(ship)
+    generateTarget()
+
+    # drawing loop
     clearCanvas()
     drawShip(ship)
     drawBullets(bullets)
+    drawTargets(targets)
+
+  updateShip = (ship) ->
+    if ship.movingLeft
+      ship.x -= ship.movementInterval if ship.x > 0
+    if ship.movingRight
+      ship.x += ship.movementInterval if (ship.x + ship.width) < canvas.width()
+    if ship.firing
+      bullets.push { width: 2, height: 2, x: ship.x + ship.width / 2, y: ship.y - 1, velocity: -15 } unless bullets.length > 4
+
+  updateBullets = ->
+    for bullet in bullets
+      bullet.y += bullet.velocity
+    bullets = (bullet for bullet in bullets when bullet.y > 0)
+
+  generateTarget = ->
+    if Math.random() < 0.01
+      targetWidth = 30
+      targets.push { width: targetWidth, height: 30, x: Math.random() * canvas.width() - targetWidth, y: 30 }
+
+  clearCanvas = ->
+    context.clearRect(0, 0, canvas.width(), canvas.height())
+
+  drawShip = (ship) ->
+    canvas.get(0).getContext("2d").fillRect(ship.x, ship.y, ship.width, ship.height)
+    canvas.get(0).getContext("2d").fillRect(ship.x + ship.width / 2 - 1, ship.y - 4, 2, 4)
+
+  drawBullets = (bullets) ->
+    for bullet in bullets
+      canvas.get(0).getContext("2d").fillRect(bullet.x, bullet.y, bullet.width, bullet.height)
+
+  drawTargets = (targets) ->
+    for target in targets
+      canvas.get(0).getContext("2d").fillRect(target.x, target.y, target.width, target.height)
+
+
 
   handleKeys = (options) -> ->
     if event.which == $.ui.keyCode.LEFT
@@ -69,5 +86,6 @@ $ ->
 
   $(document).keydown(handleKeys(down: true))
   $(document).keyup(handleKeys(down: false))
+
 
   init()
