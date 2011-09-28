56c3db79fa4862b510fa88e7c9029a737f618af9
Kyle Shipley <shipstar@gmail.com>
2011-09-27 22:14:04 -0400

    Get super-basic node server running.

---
+++ compiled/server.js
@@ -0,0 +1,50 @@
+(function() {
+  var fs, http, send404, server, sys, url;
+  sys = require('sys');
+  http = require('http');
+  fs = require('fs');
+  url = require('url');
+  server = http.createServer(function(req, res) {
+    var path;
+    path = url.parse(req.url).pathname;
+    switch (path) {
+      case '/':
+      case '/index.html':
+      case '/style.css':
+        if (path === '/') {
+          path = '/index.html';
+        }
+        path = "/.." + path;
+        return fs.readFile(__dirname + path, function(err, data) {
+          if (err) {
+            sys.puts(err);
+            return send404(res);
+          } else {
+            res.writeHead(200, 'text/html');
+            res.write(data, 'utf8');
+            return res.end();
+          }
+        });
+      case '/compiled/game.js':
+        path = "/.." + path;
+        return fs.readFile(__dirname + path, function(err, data) {
+          if (err) {
+            sys.puts(err);
+            return send404(res);
+          } else {
+            res.writeHead(200, 'text/html');
+            res.write(data, 'utf8');
+            return res.end();
+          }
+        });
+      default:
+        return send404(res);
+    }
+  });
+  send404 = function(res) {
+    res.writeHead(404);
+    res.write('404');
+    return res.end();
+  };
+  server.listen(8888);
+}).call(this);
---
+++ src/server.coffee
@@ -0,0 +1,40 @@
+sys = require 'sys'
+http = require 'http'
+fs = require 'fs'
+url = require 'url'
+
+server = http.createServer (req, res) ->
+  path = url.parse(req.url).pathname
+  switch path
+    when '/', '/index.html', '/style.css'
+      path = '/index.html' if path == '/'
+      path = "/.." + path
+      fs.readFile __dirname + path, (err, data) ->
+        if err
+          sys.puts err
+          send404(res)
+        else
+          res.writeHead 200, 'text/html'
+          res.write data, 'utf8'
+          res.end()
+
+    when '/compiled/game.js'
+      path = "/.." + path
+      fs.readFile __dirname + path, (err, data) ->
+        if err
+          sys.puts err
+          send404(res)
+        else
+          res.writeHead 200, 'text/html'
+          res.write data, 'utf8'
+          res.end()
+
+    else
+      send404 res
+
+send404 = (res) ->
+  res.writeHead 404
+  res.write '404'
+  res.end()
+
+server.listen 8888
